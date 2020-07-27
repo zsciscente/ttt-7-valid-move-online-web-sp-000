0d2a7263c3766cba5c160329cec103cf19e221b5
Zachary Sciscente <zsciscente@gmail.com>
2020-07-27 16:12:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 
 def valid_move?(board, index)
-  index.between?(0,8) || position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
  end 
  
 def position_taken?(board, index)
